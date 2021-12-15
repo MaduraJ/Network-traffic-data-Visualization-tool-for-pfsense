beefc6977ac6f219ce69e3873acd7274893c95c8
MaduraJ <56335187+MaduraJ@users.noreply.github.com>
2021-12-15 09:43:42 +0530

    Writing netTraffic data to ncapng

--- Test case 1/Ethernet-Live-Cap.py
+++ Test case 1/Ethernet-Live-Cap.py
@@ -1,24 +1,26 @@
 import pyshark
 import json
+from json import JSONEncoder
+
+
 
 try:
-	capture = pyshark.LiveCapture(interface='Ethernet')
+	capture = pyshark.LiveCapture(interface='Ethernet',output_file='tempNetTraffic.pcapng')
 	while True:
 		capture.sniff(timeout=0)
 		for packet in capture.sniff_continuously():
 			print(packet)
-			jsonString=json.dump(packet)
-			jsonFile=open("tempNetTraffic.json","w")
-			jsonFile.write(jsonString)
-			jsonFile.close()
+			#with open("tempNetTraffic.json","w") as netTraffic:
+				#json.dump(packet,netTraffic,indent=5)
+			#jsonString=json.dump(packet,indent=4)
+			#jsonFile.write(jsonString)
 			
 
 except EOFError as e:
     print(e)
 except Exception as e:
-	e
+	print(e)
 except KeyboardInterrupt:
-	jsonFile.close()
 	type(capture)
 	print("Press Ctrl-C to terminate while statement")
 else:
