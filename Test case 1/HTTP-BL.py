e29ae3c7dc4e155e2e8771aa0ad400073edb6b88
MaduraJ <56335187+MaduraJ@users.noreply.github.com>
2022-03-21 20:57:17 +0530

    Test

--- Test case 1/HTTP-BL.py
+++ Test case 1/HTTP-BL.py
@@ -1,10 +1,14 @@
 import httpbl
+import json
 ip_address='134.119.216.167'
 #100.200.159.162
 
 bl = httpbl.HttpBL('vwmjfxvftsrb')
 response = bl.query(ip_address)
 print(response)
+print(type(response))
+jsonString=json.dumps(response,indent=1)
+print(type(jsonString),"\n",response)
 #print(response.items())
 
 #print("Threat-Score: {} \nDays-since-last-activity: {}\nVisitor-Type-Number: {}".format(response['threat_score'],response['days_since_last_activity'],response['type']))
