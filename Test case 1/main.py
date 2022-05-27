3d31d334244ce8a7f8bde9b1bd630c15a8ffccc0
MaduraJ <56335187+MaduraJ@users.noreply.github.com>
2022-05-27 11:36:15 +0530

    Testing

--- Test case 1/main.py
+++ Test case 1/main.py
@@ -1,16 +1,10 @@
-import Access_token
-
-def __init__(self):
-	pass
+import Ethernet_Live_Cap, threading,gc ,re
 
 
+if __name__ == '__main__':
+	SpamANDTorNodeBlock=Ethernet_Live_Cap.TrafficCapture("Ethernet")
+	TestThread=threading.Thread(target=SpamANDTorNodeBlock.Capture)
+	gc.enable()
+	TestThread.start()
 
-headers={'Content-Type': 'application/json'}
-data='{"client-id": "admin", "client-token": "pfsense"}'
-pemCert='C:/Users/Madura1/Desktop/Python/Pfsense API/pfsense1.localdomain.pem'
-link="https://192.168.1.100/api/v1/system/arp"
-diognosticLink="https://192.168.1.100/api/v1/diagnostics/command_prompt"
-diagnosticsData='{"shell_cmd": "echo Test command"}'
 
-box1=Access_token.PfsenseRequest(link,headers,data,pemCert)
-box1.pfsnseVerify()
